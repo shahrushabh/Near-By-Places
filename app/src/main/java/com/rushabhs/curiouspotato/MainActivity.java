db0c5a95e7699e5e5830a7f784ea42172a51a984
Rushabh Shah <rvshah@iastate.edu>
2016-11-30 22:38:00 -0600

    Adding the pager and other xml layouts.

--- app/src/main/java/com/rushabhs/curiouspotato/MainActivity.java
+++ app/src/main/java/com/rushabhs/curiouspotato/MainActivity.java
@@ -1,14 +1,175 @@
 package com.rushabhs.curiouspotato;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.view.ViewPager;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity  implements GoogleApiClient.OnConnectionFailedListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments representing
+     * each object in a collection. We use a {@link android.support.v4.app.FragmentStatePagerAdapter}
+     * derivative, which will destroy and re-create fragments as needed, saving and restoring their
+     * state in the process. This is important to conserve memory and is a best practice when
+     * allowing navigation between objects in a potentially large collection.
+     */
+    CollectionPagerAdapter mCollectionPagerAdapter;
+    private GoogleApiClient mGoogleApiClient;
+
+    /**
+     * The {@link android.support.v4.view.ViewPager} that will display the object collection.
+     */
+    ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create an adapter that when requested, will return a fragment representing an object in
+        // the collection.
+        //
+        // ViewPager and its adapters use support library fragments, so we must use
+        // getSupportFragmentManager.
+        mCollectionPagerAdapter = new CollectionPagerAdapter(getSupportFragmentManager());
+
+        // Set up action bar.
+        final ActionBar actionBar = getActionBar();
+
+        // Specify that the Home button should show an "Up" caret, indicating that touching the
+        // button will take the user one step up in the application's hierarchy.
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the ViewPager, attaching the adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCollectionPagerAdapter);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This is called when the Home (Up) button is pressed in the action bar.
+                // Create a simple intent that starts the hierarchical parent activity and
+                // use NavUtils in the Support Package to ensure proper handling of Up.
+                Intent upIntent = new Intent(this, MainActivity.class);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                    // This activity is not part of the application's task, so create a new task
+                    // with a synthesized back stack.
+                    TaskStackBuilder.from(this)
+                            // If there are ancestor activities, they should be added here.
+                            .addNextIntent(upIntent)
+                            .startActivities();
+                    finish();
+                } else {
+                    // This activity is part of the application's task, so simply
+                    // navigate up to the hierarchical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A {@link android.support.v4.app.FragmentStatePagerAdapter} that returns a fragment
+     * representing an object in the collection.
+     */
+    public static class CollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        private String[] types = {"Cafe", "Airport", "Bank", "School", "Bar", "Lodging", "Pharmacy"};
+
+        public CollectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new CategoryFragment();
+            Bundle args = new Bundle();
+            args.putString(CategoryFragment.OBJECT_TYPE,types[i]);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return types.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return types[position];
+        }
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply displays dummy text.
+     */
+    public static class CategoryFragment extends Fragment {
+
+        public static final String OBJECT_TYPE = "type";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.card_list_view, container, false);
+            Bundle args = getArguments();
+            ((ListView) rootView.findViewById(R.id.list)).setAdapter(null);
+//            ((TextView) rootView.findViewById(R.id.text1)).setText(args.getString(OBJECT_TYPE));
+            Log.d("Textview value: ", args.getString(OBJECT_TYPE));
+//            Log.d("Textview value: ", ((TextView) rootView.findViewById(R.id.text1)).getText().toString());
+            return rootView;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle
+        // the failure silently
+
+        // ...
+    }
+
+    private class GetPlacesTask extends AsyncTask<String, Void, ArrayList<String>> {
+        protected ArrayList<String> doInBackground(String... s) {
+            return null;
+        }
+
+        protected void onProgressUpdate(Void... progress) {
+
+        }
+
+        protected void onPostExecute(Void result) {
+
+        }
     }
 }

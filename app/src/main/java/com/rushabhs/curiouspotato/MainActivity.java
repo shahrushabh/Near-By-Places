bbe1088f1cb39fce3cb75b20a28ec51a33c67de9
Rushabh Shah <rvshah@iastate.edu>
2016-12-01 20:37:35 -0600

    Adding ASYNCTASK and necessary components.

--- app/src/main/java/com/rushabhs/curiouspotato/MainActivity.java
+++ app/src/main/java/com/rushabhs/curiouspotato/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rushabhs.curiouspotato;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,17 +12,24 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.ViewPager;
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResult;
 import com.google.android.gms.location.places.Places;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -140,12 +148,14 @@ public class MainActivity extends FragmentActivity  implements GoogleApiClient.O
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.card_list_view, container, false);
+//            View rootView = inflater.inflate(R.layout.card_list_view, container, false);
+            View rootView = inflater.inflate(R.layout.card_object_view, container, false);
             Bundle args = getArguments();
-            ((ListView) rootView.findViewById(R.id.list)).setAdapter(null);
-//            ((TextView) rootView.findViewById(R.id.text1)).setText(args.getString(OBJECT_TYPE));
+//            ((ListView) rootView.findViewById(R.id.list)).setAdapter(null);
+            ((TextView) rootView.findViewById(R.id.text1)).setText(args.getString(OBJECT_TYPE));
             Log.d("Textview value: ", args.getString(OBJECT_TYPE));
-//            Log.d("Textview value: ", ((TextView) rootView.findViewById(R.id.text1)).getText().toString());
+            ImageView img = ((ImageView) rootView.findViewById(R.id.placeImage));
+//            new GetPlacesTask(img.getMaxWidth(), img.getMaxHeight()).execute("ChIJrTLr-GyuEmsRBfy61i59si0");
             return rootView;
         }
     }
@@ -159,17 +169,71 @@ public class MainActivity extends FragmentActivity  implements GoogleApiClient.O
         // ...
     }
 
-    private class GetPlacesTask extends AsyncTask<String, Void, ArrayList<String>> {
-        protected ArrayList<String> doInBackground(String... s) {
-            return null;
+    private class GetPlacesTask extends AsyncTask<String, Void, GetPlacesTask.AttributedPhoto> {
+        private int maxHeight;
+        private int maxWidth;
+        public GetPlacesTask(int width, int height){
+            maxWidth = width;
+            maxHeight = height;
+        }
+        protected AttributedPhoto doInBackground(String... s) {
+            if(s.length != 1){return null;}
+            final String placeId = s[0];
+            AttributedPhoto attributedPhoto = null;
+
+            PlacePhotoMetadataResult result = Places.GeoDataApi
+                    .getPlacePhotos(mGoogleApiClient, placeId).await();
+
+            if (result.getStatus().isSuccess()) {
+                PlacePhotoMetadataBuffer photoMetadataBuffer = result.getPhotoMetadata();
+                if (photoMetadataBuffer.getCount() > 0 && !isCancelled()) {
+                    // Get the first bitmap and its attributions.
+                    PlacePhotoMetadata photo = photoMetadataBuffer.get(0);
+                    CharSequence attribution = photo.getAttributions();
+                    // Load a scaled bitmap for this photo.
+                    Bitmap image = photo.getScaledPhoto(mGoogleApiClient, maxWidth, maxHeight).await()
+                            .getBitmap();
+
+                    attributedPhoto = new AttributedPhoto(attribution, image);
+                }
+                // Release the PlacePhotoMetadataBuffer.
+                photoMetadataBuffer.release();
+            }
+            return attributedPhoto;
         }
 
         protected void onProgressUpdate(Void... progress) {
-
+            // Nothing onProgress
         }
 
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(AttributedPhoto result) {
+            if(result != null){
+                TextView mText = ((TextView) findViewById(R.id.text1));
+                // Successfully downloaded the photo. Display it on the screen.
+                ((ImageView) findViewById(R.id.placeImage)).setImageBitmap(result.bitmap);
+                // Display the attribution as HTML content if set.
+                if (result.attribution == null) {
+                    mText.setVisibility(View.GONE);
+                } else {
+                    mText.setVisibility(View.VISIBLE);
+                    mText.setText(Html.fromHtml(result.attribution.toString()));
+                }
+            }
+        }
 
+        /**
+         * Holder for an image and its attribution.
+         */
+        class AttributedPhoto {
+
+            public final CharSequence attribution;
+
+            public final Bitmap bitmap;
+
+            public AttributedPhoto(CharSequence attribution, Bitmap bitmap) {
+                this.attribution = attribution;
+                this.bitmap = bitmap;
+            }
         }
     }
 }
